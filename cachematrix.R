182e14f500ed87d8a0494a0cefd8871efcca08e6
Glauber Cantacini Romano <gcantacini@hotmail.com>
2018-06-04 00:00:47 -0300

    Programming Assignment 2: Lexical Scoping
    Submission to Coursera's R Programming course - Programming Assignment 2 (Week 3)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function responsible for constructing cache object for a specified matrix 
+## and its respective inverse matrix, when extracted
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    storedInverseMatrix <- NULL
+    set <- function(matrix) {
+        x <<- matrixValue
+        storedInverseMatrix <<- NULL
+    }
+    get <- function() x
+    setInverseMatrix <- function(inverseMatrix) storedInverseMatrix <<- inverseMatrix
+    getInverseMatrix <- function() storedInverseMatrix
+    list(set = set, 
+         get = get, 
+         setInverseMatrix = setInverseMatrix, 
+         getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## Function responsible for receiving a cache object and returning the inverse matrix
+## from the matrix stored in the cache object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    storedInverseMatrix <- x$getInverseMatrix()
+    if(!is.null(storedInverseMatrix)) {
+        message("Getting cached data")
+        return(storedInverseMatrix)
+    }
+    data <- x$get()
+    inverseMatrix <- tryCatch.W.E(solve(data, ...))
+    if (is.matrix(inverseMatrix$value)) {
+        storedInverseMatrix <- inverseMatrix$value
+        x$setInverseMatrix(storedInverseMatrix)
+    }
+    else {
+        message("The matrix is non-invertible")
+    }
+    storedInverseMatrix
 }
